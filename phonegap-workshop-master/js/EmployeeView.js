17da854089f981a8941eac40b4b681d20894afa6
Jeffery Finley <jeff@jefferyfinley.com>
2014-11-01 13:43:53 -0500

    Working on the geolocation.  It's not working using Ripple so I'm pushing to the repo to test on my actual phone

--- phonegap-workshop-master/js/EmployeeView.js
+++ phonegap-workshop-master/js/EmployeeView.js
@@ -1,7 +1,19 @@
 var EmployeeView = function(employee) {
- 
+	this.addLocation = function(event) {
+	    event.preventDefault();
+	    console.log('addLocation');
+	    navigator.geolocation.getCurrentPosition(
+	        function(position) {
+	            $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+	        },
+	        function() {
+	            alert('Error getting location');
+	        });
+	    return false;
+	};
     this.initialize = function() {
         this.el = $('<div/>');
+		this.el.on('click', '.add-location-btn', this.addLocation);
     };
  
 	this.render = function() {
